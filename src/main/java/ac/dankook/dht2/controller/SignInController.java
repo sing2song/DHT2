3b00cab20ccf623f85d3c392a0f2989bf92b8cb0
eunchong <chd830@naver.com>
2018-08-20 19:33:24 +0900

    로그인 완료

--- src/main/java/ac/dankook/dht2/controller/SignInController.java
+++ src/main/java/ac/dankook/dht2/controller/SignInController.java
@@ -19,17 +19,18 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("/")
 public class SignInController {
-    @Autowired private UserService userService;
+    @Autowired
+    private UserService userService;
     private static final Logger LOGGER = LoggerFactory.getLogger(SignInController.class);
 
     @RequestMapping("")
     public String index() {
         return "signin";
     }
+
     @RequestMapping("signin")
     @ResponseBody
-    public  String signIn(@RequestParam String user_id, @RequestParam String user_password, HttpServletResponse response) {
-        LOGGER.debug("Controller");
+    public String signIn(@RequestParam String user_id, @RequestParam String user_password, HttpServletResponse response) {
         Cookie cookie = new Cookie("name", user_id);
         cookie.setMaxAge(60 * 60 * 24);
         response.addCookie(cookie);
@@ -37,40 +38,13 @@ public class SignInController {
         user.setUser_id(user_id);
         user.setUser_password(user_password);
         Boolean result = userService.checkPassword(user);
+        LOGGER.debug("checkPassword");
         if (result) {
             LOGGER.debug("Success");
-            return user_id;
+            return "input";
         } else {
             LOGGER.debug("failed");
-            return "fail";
+            return null;
         }
     }
-
-
-//    @RequestMapping("signin")
-//    public String signin(@RequestParam String user_id, @RequestParam String user_password) {
-//        LOGGER.debug(user_id);
-//        LOGGER.debug(user_password);
-//
-//        return "redirect:/average";
-//    }
-//    @RequestMapping(method = RequestMethod.GET)
-//    public void testCookie(HttpServletResponse response) {
-//        Cookie setCookie = new Cookie("name", "test");
-//        setCookie.setMaxAge(60 * 60 * 1);
-//        response.addCookie(setCookie);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.GET)
-//    public void testCookie(HttpServletRequest request) {
-//        Cookie[] getCookie = request.getCookies();
-//        if (getCookie != null) {
-//            for (int i = 0; i < getCookie.length; i++) {
-//                Cookie c = getCookie[i];
-//                String name = c.getName();
-//                String value = c.getValue();
-//            }
-//        }
-//    }
 }
-
